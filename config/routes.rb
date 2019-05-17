a6480a27048edd94ee2c3c229851e2e19f9cd6ec
Pat <patrick.s.santucci@gmail.com>
2019-05-17 13:20:40 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index, :show]
+  resources :posts, only: %i[index show]
+  get '/register', to: 'users#new', as: 'register'
 end
